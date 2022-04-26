48cd33da6b558abf467c80b57a6c08fdf740fda2
Satrey <satrey.mail@gmail.com>
2022-04-26 10:06:17 +0500

    Make model Post
    Add model Post and make migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Post(models.Model):
 
+    title  = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        'auth.user',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.title
