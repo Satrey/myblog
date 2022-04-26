f7881035b45ce5299bc49b456cf65672d99fa39d
Satrey <satrey.mail@gmail.com>
2022-04-26 10:21:14 +0500

    Change localization
    Add ru local, add new posts to db. Change fields names in adminsite.

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Post(models.Model):
 
-    title  = models.CharField(max_length=50)
+    title  = models.CharField(max_length=50, verbose_name='Заголовок статьи')
     author = models.ForeignKey(
         'auth.user',
         on_delete=models.CASCADE,
+        verbose_name='Автор'
     )
-    body = models.TextField(max_length=500)
+    body = models.TextField(max_length=500, verbose_name='Текст')
 
     def __str__(self) -> str:
         return self.title
