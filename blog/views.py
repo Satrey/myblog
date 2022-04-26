3ba3e4383893b9c888d41d75a364cd91a5741e54
Satrey <satrey.mail@gmail.com>
2022-04-26 17:14:39 +0500

    Added delete.html form functional

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
+from audioop import reverse
 from attr import fields
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView, UpdateView
-from matplotlib.pyplot import title
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 from .models import Post
 
@@ -28,4 +30,10 @@ class BlogEditView(UpdateView):
      model = Post
      template_name = 'edit.html'
      fields = ['title', 'body']
-     
+
+class BlogDeleteView(DeleteView):
+
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('home')
+    
