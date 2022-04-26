067e30666b53c15231c94573fa7e39ac2b61c68c
Satrey <satrey.mail@gmail.com>
2022-04-26 16:20:22 +0500

    Added edit form

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>', views.BlogDetailView.as_view(), name='detail'),
-    path('post/new/', views.BlogCreateView.as_view(), name='create')
+    path('post/new/', views.BlogCreateView.as_view(), name='create'),
+    path('post/<int:pk>/edit/', views.BlogEditView.as_view(), name='edit')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from attr import fields
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from matplotlib.pyplot import title
 
 from .models import Post
 
@@ -20,3 +22,10 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'create.html'
     fields = '__all__'
+
+class BlogEditView(UpdateView):
+
+     model = Post
+     template_name = 'edit.html'
+     fields = ['title', 'body']
+     
