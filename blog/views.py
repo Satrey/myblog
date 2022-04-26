5e444c1cb9b8773a2048acbd2e60cc261d289c7b
Satrey <satrey.mail@gmail.com>
2022-04-26 14:48:10 +0500

    Add form for new post.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -13,3 +14,9 @@ class BlogDetailView(DetailView):
 
     model = Post
     template_name = 'detail.html'
+
+class BlogCreateView(CreateView):
+
+    model = Post
+    template_name = 'create.html'
+    fields = '__all__'
